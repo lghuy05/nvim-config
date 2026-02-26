d690b3e225b3745d54b068decaeeb16589456624
lghuy05 <luonggiahuy1909@gmail.com>
2026-02-26 15:29:38 -0500

    new config

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -1,17 +1,27 @@
 return {
-  "folke/flash.nvim",
-  enabled = true,
-  keys = {
-    -- Don't define flash's 's' keymap at all
-  },
-  opts = {
-    modes = {
-      char = {
-        enabled = false, -- disable for f/F/t/T
-      },
-      search = {
-        enabled = false, -- also disable search mode to prevent any interference
+  {
+    "folke/flash.nvim",
+    enabled = true,
+    keys = {
+      -- Don't define flash's 's' keymap at all
+    },
+    opts = {
+      modes = {
+        char = {
+          enabled = false, -- disable for f/F/t/T
+        },
+        search = {
+          enabled = false, -- also disable search mode to prevent any interference
+        },
       },
     },
+  },
+  {
+    "folke/noice.nvim",
+    opts = function(_, opts)
+      opts.cmdline = {
+        view = "cmdline",
+      }
+    end,
   },
 }
